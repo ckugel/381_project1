15534b94ff68bbd5c3c18b0d69dc17b1f5bd7bfd
ckugel <calebkugel1@gmail.com>
2025-03-12 17:33:53 -0500

    Top level entity

--- proj/src/TopLevel/MIPS_Processor.vhd
+++ proj/src/TopLevel/MIPS_Processor.vhd
@@ -33,7 +33,6 @@ end  MIPS_Processor;
 
 
 architecture structure of MIPS_Processor is
-
   -- Required data memory signals
   signal s_DMemWr       : std_logic; -- TODO: use this signal as the final active high data memory write enable signal
   signal s_DMemAddr     : std_logic_vector(N-1 downto 0); -- TODO: use this signal as the final data memory address input
@@ -56,6 +55,43 @@ architecture structure of MIPS_Processor is
   -- Required overflow signal -- for overflow exception detection
   signal s_Ovfl         : std_logic;  -- TODO: this signal indicates an overflow exception would have been initiated
 
+    signal currentPC : std_logic_vector(31 downto 0); -- coming out of the PC register
+    signal nextPC : std_logic_vector(31 downto 0); -- coming out of the mux on the top right in diagram
+    signal PCPlus4 : std_logic_vector(31 downto 0); -- coming out of the PC+4 incrementer
+    signal jumpAddress : std_logic_vector(31 downto 0); -- coming out of the shifter with pc + 4 being the top four bits
+    signal aluJump : std_logic_vector(31 downto 0); -- coming out of the jump adder
+
+    signal immediate_shifted_2 : std_logic_vector(31 downto 0); -- coming out of the shifter for the jump adder
+    signal aluZeroAndBranch : std_logic;
+
+	signal writeReg : std_logic_vector(4 downto 0); -- coming out of the mux for the register file
+
+
+	signal regDst : std_logic;
+	signal jump : std_logic;
+	signal branch : std_logic;
+	signal memRead : std_logic;
+	signal memtoReg : std_logic;
+	signal aluOp : std_logic_vector(1 downto 0);
+	signal memWrite : std_logic;
+	signal aluSrc : std_logic;
+	signal regWrite : std_logic;
+
+	signal write_data_reg : std_logic_vector(31 downto 0);
+	signal reg_o_s : std_logic_vector(31 downto 0);
+	signal reg_o_t : std_logic_vector(31 downto 0);
+	signal s_o_zero : std_logic;
+
+	signal immeditate_extended : std_logic_vector(31 downto 0);
+	signal dmem_read_data : std_logic_vector(31 downto 0);
+	signal ALU_B : std_logic_vector(31 downto 0); -- ALU second input either port s or immediate depending on mux
+	signal ALU_operation : std_logic_vector(31 downto 0); -- post ALU op controller into ALU
+
+	 
+	signal s_jump_high_mux_in : std_logic_vector(31 downto 0);
+
+
+
   component mem is
     generic(ADDR_WIDTH : integer;
             DATA_WIDTH : integer);
@@ -67,8 +103,122 @@ architecture structure of MIPS_Processor is
           q            : out std_logic_vector((DATA_WIDTH -1) downto 0));
     end component;
 
-  -- TODO: You may add any additional signals or components your implementation 
-  --       requires below this comment
+    component ALU is
+		port(
+			A : in std_logic_vector(31 downto 0);
+			B : in std_logic_vector(31 downto 0);
+			ALUControl : in std_logic_vector(3 downto 0);
+			carryOut : out std_logic;
+			overflow : out std_logic;
+			result : out std_logic_vector(31 downto 0);
+			zero : out std_logic
+		);
+	end component;
+
+	component RegisterFile is
+		port(
+			i_rs : in std_logic_vector(4 downto 0); -- source register
+			i_rt : in std_logic_vector(4 downto 0); -- source register
+			i_rd : in std_logic_vector(4 downto 0); -- destination register
+			i_wd : in std_logic_vector(31 downto 0); -- data to write
+			i_we : in std_logic;
+			o_s : out std_logic_vector(31 downto 0); -- output of register specified by i_rs
+			o_t : out std_logic_vector(31 downto 0) -- output of register specified by i_rt
+		);
+	end component;
+
+	component ControlUnit is
+		port(
+			funct : in std_logic_vector(6 downto 0);
+			opcode : in std_logic_vector(6 downto 0);
+			RegDst : out std_logic;
+			Jump : out std_logic;
+			Branch : out std_logic;
+			MemRead : out std_logic;
+			MemtoReg : out std_logic;
+			ALUOp : out std_logic_vector(2 downto 0);
+			MemWrite : out std_logic;
+			ALUSrc : out std_logic;
+			RegWrite : out std_logic
+		);
+	end component;
+
+	component ProgramCounter is
+		port(
+			clk : in std_logic;
+			reset : in std_logic;
+			enable : in std_logic;
+			PC : out std_logic_vector(31 downto 0)
+		);
+	end component;
+
+	component ProgramCounterIncrementer is
+		port(
+			PC : in std_logic_vector(31 downto 0);
+			PCPlus4 : out std_logic_vector(31 downto 0)
+		);
+	end component;
+
+	component shifter_2 is
+		port(
+			input : in std_logic_vector(31 downto 0);
+			output : out std_logic_vector(31 downto 0)
+		);
+	end component;
+
+	component reg_n is
+	    generic(n : integer : 32);
+	    port(
+		    i_CLK : std_logic;
+		    i_RST : std_logic;
+		    i_WE : std_logic;
+		    i_Data : std_logic_vector(n - 1 downto 0);
+		    o_Q : std_logic_vector(n-1 downto 0)
+		);
+
+	component mux2t1_N is
+	    generic( N : integer := 16);
+	    port(
+		i_S : in std_logic;
+		i_D0 : in std_logic_vector(N-1 downto 0);
+		i_D1 : in std_logic_vector(N-1 downto 0);
+		o_Q : out std_logic_vector(N-1 downto 0)
+	    );
+    	end component;
+
+	component sign_extender_16_32 is
+	    port(
+		input : in std_logic_vector(15 downto 0);
+		output : out std_logic_vector(31 downto 0)
+	    );
+	end component;
+
+	component andg2 is 
+	    port (
+		i_A : in std_logic;
+		i_B : in std_logic;
+		o_F : out std_logic;
+		    );
+
+	component Adder is
+	    generic(N : integer := 16);
+	    port(
+	    Cin : in std_logic;
+	    i_A : in std_logic_vector(N-1 downto 0);
+	    i_B : in std_logic_vector(N-1 downto 0);
+	Cout : out std_logic;
+	    o_sum : out std_logic_vector(N-1 downto 0)
+	    );
+	end component;
+
+
+	component shifterJump is
+	    port(
+		instruction : in std_logic_vector(31 downto 0);
+		PCPlus4 : in std_logic_vector(31 downto 0);
+		output : out std_logic_vector(31 downto 0)
+	    );
+	end component;
 
 begin
 
@@ -99,7 +249,142 @@ begin
   -- TODO: Ensure that s_Halt is connected to an output control signal produced from decoding the Halt instruction (Opcode: 01 0100)
   -- TODO: Ensure that s_Ovfl is connected to the overflow output of your ALU
 
-  -- TODO: Implement the rest of your processor below this comment! 
+    MainALU: ALU
+	port map(
+		A =>  reg_o_t,
+		B => ALU_B,
+		ALUControl => ALU_operation,
+		carryOut => s_carryOut,
+		overflow => s_Ovfl,
+		result => oALUOut,
+		zero => s_o_zero,
+	    );
+	-- define the second input (B) for the ALU
+	ALU_B_mux : mux2t1_N
+	generic map(N => 32);
+	port map(
+		i_S => aluSrc,
+		i_D0 => reg_o_t,
+		i_D1 => immeditate_extended,
+		o_Q => ALU_B
+		);
+
+	-- define the control unit
+	controlUnit : ControlUnit
+	port map(
+		opcode => s_Inst(31 downto 26),
+		RegDst => regDst,
+		Jump => jump,
+		Branch => branch,
+		MemRead => memRead,
+		MemtoReg => memtoReg,
+		ALUOp => aluOp,
+		MemWrite => memWrite,
+		ALUSrc => aluSrc,
+		RegWrite => regWrite
+		);
+
+	-- define the mux for the ALU result or the data memory read data
+	memToRegMux : mux2t1_N
+	generic map(N => 32);
+	port map(
+	i_S => memtoReg,
+	i_D0 => ALU_result,
+	i_D1 => dmem_read_data,
+	o_Q => write_data_reg
+	);
+
+	-- define the mux for which register to write to
+	regDstMux : mux2t1_N
+	generic map(N => 5);
+	port map(
+		i_S => regDst,
+		i_D0 => s_Inst(20 downto 16),
+		i_D1 => s_Inst(15 downto 11),
+		o_Q => writeReg
+		);
+
+	-- define the sign extender
+	signExtender : sign_extender_16_32
+	port map(
+		input => s_Inst(15 downto 0),
+		output => immeditate_extended
+		);
+
+	-- Define the program counter
+	programCounterReg : reg_n
+	generic map(n => 32);
+	port map(
+		    i_CLK => iCLK,
+		    i_RST => iRST,
+		    i_WE => '1',
+		    i_Data => s_NextInstAddr,
+		    o_Q => currentPC
+		);
+	-- define program counter incrementer
+	PCInc : ProgramCounterIncrementer
+	port map(
+		PC => currentPC,
+		PCPlus4 => PCPlus4
+		);
+
+
+-- define the mux for next PC
+-- the select value in the mux should be from a control signal
+-- The high value should be the shifted instruction 25 downto 0 bits and the top four bits should be the PC + 4
+	shifter_jumper : shifterJump
+	port map(
+		instruction => s_Inst,
+		PCPlus4 => PCPlus4,
+		output => s_jump_high_mux_in
+		);
+
+-- define the mux for the ALU second input into jump address
+    preNextPCmux : mux2t1_N
+    generic map(N => 32);
+    port map(
+	i_S => aluZeroAndBranch,
+	i_D0 => nextPC,
+	i_D1 => aluJump,
+	o_Q => jumpAddress
+	);
+
+    nextPCmux : mux2t1_N
+    generic map(N => 32);
+    port map(
+	i_S => jump,
+	i_D0 => PCPlus4,
+	i_D1 => jump_address,
+	o_Q => nextPC
+    );
+
+	-- define the jump adder
+    jumpAdder : Adder 
+    generic map(N => 32);
+    port map(
+	Cin => '0',
+	i_A nextPC,
+	i_B => immediate_shifted_2,
+	Cout => open,
+	o_sum => aluJump
+    );
+
+
+    -- shifter for the jump alu
+    shifter2_1 : shifter_2
+    port map(
+	input => immediate_extended,
+	output => immediate_shifted_2
+	);
+
+    -- define the and gate which makes up the select line for the second mux
+    andGate : andg2
+    port map(
+    i_A => branch,
+    i_B => s_o_zero,
+    o_F => aluZeroAndBranch
+	);
+
 
 end structure;
 
